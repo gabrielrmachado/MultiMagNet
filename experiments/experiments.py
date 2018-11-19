17ee1e4fc5c3433a1b1101a0b07d217ae6aeac3a
Gabriel Machado <gabriel.rmachado10@gmail.com>
2018-11-19 12:32:13 -0200

    'choose_team_each_jump_experi ment' method updated

--- experiments/experiments.py
+++ experiments/experiments.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import utils.helpers as helpers
+import random
 
 from utils.data import Data
 from modules.retrieve_module import Retrieval
@@ -197,7 +198,7 @@ class Experiment:
             print("LEG < ADV: {0} / {1}".format(len(np.where(leg < adv)[0]), length))
             del autoencoder
 
-    def simple_experiment(self, reduction_models = 3, attack="FGSM", drop_rate=0.001, tau="RE", p = 1, length=2000, T=1, metric='JSD'):
+    def simple_experiment(self, reduction_models, attack="FGSM", drop_rate=0.001, tau="RE", p = 1, length=2000, T=1, metric='JSD'):
         """
         Evaluates MultiMagNet with test dataset containing half legitimate and adversarial images, and prints the its metrics.
 
@@ -262,87 +263,75 @@ class Experiment:
 
         print("\nExperiment's elapsed time: {0}".format(timedelta(seconds=time.time() - start)))
 
-    def choose_team_each_jump_experiment(self, n_experiments, reduction_models, attack, drop_rate, tau, jump = 50,  length=2000, p = 1):
-
+    def choose_team_each_jump_experiment(self, jump=0, magnet=False, attack="FGSM", drop_rate=0.001, tau="RE", p = 1, length=2000, T=1, metric='JSD'):
         """
-        Pìcks randomly different autoencoders for each jump and prints the final result.
+        Evaluates MultiMagNet with test dataset containing half legitimate and adversarial images, and prints the its metrics.
 
-        # Attributes:
-            n_experiments: the number of experiments that will be performed.
-            
+        # Attributes:        
             length: the size of the test dataset containing legitimate images that will be used in the experiments. A final test dataset will be produced containing legitimate and adversarial images, with size length * 2.
 
             jump: forms a different 'R' team at each jump.
-            
-            reduction_models: the number of autoencoders randomly chosen to form the MultiMagNet ensemble, which will be compared to its one-autoencoder version afterwards. 
+
+            magnet: if True, it is chosen just one autoencoder; if False, it is chosen a random number of autoencoders.
 
             attack: can be 'FGSM', 'BIM', 'DEEPFOOL', 'CW_0.0', 'CW_10.0', 'CW_20.0', 'CW_30.0', 'CW_40.0'.
 
             drop_rate: the maximum percentage of legitimate images classified as 'adversarial'.
 
-            tau: the approach used to compute the thresholds. It can be 'RE' which assigns a different threshold based on each autoencoder's reconstruction error or 'minRE', which assigns the minimum reconstruction error obtained for all the autoencoders.
+            tau: the approach used to compute the thresholds. It can be 'RE' which assigns a different threshold based on each autoencoder's reconstruction error or 'minRE', which assigns the minimum reconstruction error obtained for all the autoencoders. 
         """
         start = time.time()
 
-        team_stats = np.zeros((n_experiments, 5))
-        unique_stats = np.zeros((n_experiments, 5))
-
         # test inputs on main classifier
-        classifier = Classifier(self._sess, self._data, epochs=20)
-        model = classifier.execute()
+        classifier = Classifier(self._sess, self._data, epochs=350, learning_rate=0.01, batch_size=32)
+        classifier.execute()
+        n_autoencoders = []
 
-        # Creates surrogate model and returns the perturbed NumPy test set  
-        x_test_adv = Adversarial_Attack(self._sess, self._data, length=length, attack=attack, epochs=5).attack(helpers.get_logits(classifier.model))
+        # # Creates surrogate model and returns the perturbed NumPy test set  
+        x_test_adv = Adversarial_Attack(self._sess, self._data, length=length, attack=attack, epochs=12).attack(model=classifier.model)
 
         # Evaluates the brand-new adversarial examples on the main model.
-        scores = model.evaluate(x_test_adv[:length], self._data.y_test[self._idx_adv][:length], verbose=0)
-        print("\nMain classifier's baseline error: %.2f%%" % (100-scores[1]*100))
+        scores_leg = classifier.model.evaluate(self._data.x_test[self._idx_adv][:length], self._data.y_test[self._idx_adv][:length], verbose=1)
+        scores = classifier.model.evaluate(x_test_adv[:length], self._data.y_test[self._idx_adv][:length], verbose=1)
+        print("\nMain classifier's accuracy on legitimate examples: %.2f%%" % (scores_leg[1]*100))
+        print("\nMain classifier's accuracy on adversarial examples: %.2f%%" % (scores[1]*100))
 
         # plots the adversarial images
-        #helpers.plot_images(data.x_test[idx_adv], x_test_adv, x_test_adv.shape)
+        #helpers.plot_images(self._data.x_test[self._idx_adv][:length], x_test_adv[:length], x_test_adv.shape)
 
         # Creates a test set containing 'length * 2' input images 'x', where half are benign images and half are adversarial.
-        _, x, y = helpers.join_test_sets(self._data.x_test, x_test_adv, length)
+        _, x, y, y_ori = helpers.join_test_sets(self._data, x_test_adv, length, idx=self._idx_adv[:length])
 
-        for exp in range(n_experiments):
-            confusion_matrix_team = np.zeros((2,2))
-            confusion_matrix_unique = np.zeros((2,2))
-
-            unique_autoencoder = Assembly_Team(self._sess, self._data, 1)
-            multiple_team = Assembly_Team(self._sess, self._data, reduction_models)
-
-            # predicted labels
-            y_pred_team = np.zeros((len(y)))
-            y_pred_unique = np.zeros((len(y)))
-
-            # Forms different 'R' teams for each input image 'x'
-            for i in range(len(x)):
-                print("\nInput image 'x' {0}/{1}".format(i+1, len(x)))
+        for i in range(len(x)):
+            if i % jump == 0:
+                reduction_models = random.choice([3,5,7,9]) if not magnet else 1
+                print("\nInput images 'x' {0}-{1}/{2}\nNumber of autoencoders chosen: {3}".format(i+1, i+jump, len(x), reduction_models))
                 print("==============================================")
+                n_autoencoders.append(reduction_models)
+                team = Assembly_Team(self._sess, self._data, reduction_models)
+                
+                if metric == "RE":
+                    thresholds = team.get_thresholds(tau=tau, drop_rate=drop_rate, p = p, plot_rec_images=False)
+                    x_marks = Image_Reduction.apply_techniques(x, team, p = p)
+                else:
+                    thresholds = team.get_thresholds_pd(tau=tau, classifier = classifier, T=T, drop_rate=drop_rate, p = p, plot_rec_images=False, metric=metric)
+                    x_marks = Image_Reduction.apply_techniques_pd(x, team, classifier, T=T, p = p, metric=metric)
 
-                if i % jump == 0:
-                    thresholds = multiple_team.get_thresholds(self._data.x_val, tau=tau, drop_rate=drop_rate, p = p, plot_rec_images=False)
-                    threshold = unique_autoencoder.get_thresholds(self._data.x_val, tau=tau, drop_rate=drop_rate, p = p, plot_rec_images=False)
+        y_pred, filtered_indices = poll_votes(x, y, x_marks, thresholds, reduction_models)
 
-                x_ = x[i].reshape(1, x.shape[1], x.shape[2], x.shape[3])
+        print("\nEXPERIMENT USING {0} DATASET: {1} Input Images 'x', {2} Attack, p = {3}, reduction models = {4}, drop_rate = {5}\n, T = {6}"
+        .format(self._data.dataset_name, len(x), attack, p, reduction_models, drop_rate, T))
 
-                x_marks = Image_Reduction.apply_techniques(x_, multiple_team, p = p)
-                x_marks_u = Image_Reduction.apply_techniques(x_, unique_autoencoder, p = p)
+        acc, pp, nn, auc, f1, cm = helpers.get_cm_and_statistics(y, y_pred)
 
-                y_pred_team[i] = poll_votes_each_x(x_, y[i], x_marks, thresholds, reduction_models)
-                y_pred_unique[i] = poll_votes_each_x(x_, y[i], x_marks_u, threshold, 1)
-            
-            team_stats[exp,0], team_stats[exp,1], team_stats[exp,2], team_stats[exp,3], team_stats[exp,4], confusion_matrix_team = helpers.get_cm_and_statistics(y, y_pred_team)        
-            unique_stats[exp,0], unique_stats[exp,1], unique_stats[exp,2], unique_stats[exp,3], unique_stats[exp,4], confusion_matrix_unique = helpers.get_cm_and_statistics(y, y_pred_unique)   
+        print('Threshold used: {0}\nConfusion Matrix:\n{1}\nACC: {2}, Positive Precision: {3}, Negative Precision: {4}, AUC: {5:.3}, F1: {6:.3}'
+            .format(thresholds, cm, acc, pp, nn, auc, f1))
 
-            print("\nExperiment {0}/{1}\nTeam CM \n{2}\nOne Autoencoder CM:\n{3}"
-                        .format(exp+1, n_experiments, confusion_matrix_team, confusion_matrix_unique))
+        ori_acc, ref_acc = Reformer(classifier.model, team, x[filtered_indices], y_ori[filtered_indices])
+        d_acc = classifier.model.evaluate(x, y_ori)[1]
 
-        print("\nEXPERIMENT 2 TERMINATED. {0} DATASET: {1} Input Images 'x', {2} Attack, p = {3}, reduction models = {4}, drop_rate = {5}\n"
-            .format(self._data.dataset_name, len(x), attack, p, reduction_models, drop_rate))
-
-        helpers.get_statistics_experiments("Team", team_stats)
-        helpers.get_statistics_experiments("Unique", unique_stats)
+        print("\nModel accuracy on D set: %.2f%%" % (d_acc*100))
+        print("\nModel accuracy on filtered images: %.2f%%" % (ori_acc*100))
+        print("Model accuracy on filtered and reformed images: %.2f%%" % (ref_acc*100))
 
         print("\nExperiment's elapsed time: {0}".format(timedelta(seconds=time.time() - start)))
-
--- main.py
+++ main.py
@@ -2,17 +2,17 @@ from experiments.experiments import Experiment
 
 def simple_experiment(dataset):
     exp = Experiment(dataset)
-    exp.simple_experiment(reduction_models=5, attack="CW_40.0", drop_rate=0.001, tau="minRE", length=2000, T=5, metric="RE")
+    exp.simple_experiment(attack="DEEPFOOL", drop_rate=0.07, tau="minRE", length=2000, T=1, metric="JSD")
     
 def choose_team_each_jump_experiment(dataset):
     exp = Experiment(dataset)
-    exp.choose_team_each_jump_experiment(n_experiments=5, reduction_models=3, attack="CW_10.0", drop_rate=0.01, 
-        tau="RE", jump=50, length=2000)
+    exp.choose_team_each_jump_experiment(jump=10, magnet=False, attack="CW_10.0", drop_rate=0.01, 
+        tau="RE", length=2000)
 
 def all_cases_experiment(dataset):
     exp = Experiment(dataset)
-    # attacks = ["FGSM", "BIM", "DEEPFOOL", "CW_0.0"]
-    attacks = ["CW_40.0"]
+    #attacks = ["FGSM", "BIM", "DEEPFOOL", "CW_0.0"]
+    attacks = ["CW"]
     drop_rate = [0.001]
     reduction_models = [1,3,5,7,9]
     tau = ["RE", "minRE"]
@@ -21,8 +21,7 @@ def all_cases_experiment(dataset):
 
     exp.all_cases_experiment([5], reduction_models, attacks, drop_rate, tau, T, metric)
 
-simple_experiment("MNIST")
-
-# executes chosen experiment
-#all_cases_experiment("MNIST")
+#simple_experiment("CIFAR")
+#all_cases_experiment("CIFAR")
+choose_team_each_jump_experiment("MNIST")
 
